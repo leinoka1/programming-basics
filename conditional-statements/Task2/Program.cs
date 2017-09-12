a6dadde1dd423538f443552116084b21d8b02bf1
leinoka1 <kari.v.leino@gmail.com>
2017-09-12 13:56:23 +0300

    Added Task2

--- conditional-statements/Task2/Program.cs
+++ conditional-statements/Task2/Program.cs
@@ -6,7 +6,35 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Prompt user
+            Console.Write("Please enter number");
+
+            // Read user input
+            string userInput;
+            userInput = Console.ReadLine();
+
+            // Evaluate user input
+
+            int evaluatedNumber;
+            // evaluatedNumber = int.Parse(userInput);
+            int.TryParse(userInput, out evaluatedNumber);
+
+            Console.WriteLine("User input was: {0} : integer: {1}", userInput, evaluatedNumber);
+            // Console.WriteLine("user input was: " + userInput);
+
+
+            if (evaluatedNumber % 2 == 0)
+            {
+                Console.WriteLine("Number {0} is even", evaluatedNumber);
+            }
+
+            else
+            {
+                Console.WriteLine("Number {0} is odd", evaluatedNumber);
+            }
+
+            // Wait for user input
+            Console.ReadKey();
         }
     }
 }
