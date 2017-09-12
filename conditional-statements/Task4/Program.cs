d2e02466233c385bb9a1b2a5ca9f0baa069f0fe7
leinoka1 <kari.v.leino@gmail.com>
2017-09-12 15:18:18 +0300

    Added Task4

---
+++ conditional-statements/Task4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user
+            // Read user first input
+            Console.Write("Please enter first number");
+                       
+            string userInputFirst;
+            int evaluatedNumberFirst;
+            userInputFirst = Console.ReadLine();
+            int.TryParse(userInputFirst, out evaluatedNumberFirst);
+
+            // Read user second input
+            Console.Write("Please enter second number");
+                        
+            string userInputSecond;
+            int evaluatedNumberSecond;
+            userInputSecond = Console.ReadLine();
+            int.TryParse(userInputSecond, out evaluatedNumberSecond);
+
+            // Read user third input
+            string userInputThird;
+            int evaluatedNumberThird;
+            Console.Write("Please enter third number");
+            userInputThird = Console.ReadLine();
+            int.TryParse(userInputThird, out evaluatedNumberThird);
+            
+            // Evaluate user input   
+            
+            if (evaluatedNumberFirst < evaluatedNumberSecond)
+
+            {
+                if (evaluatedNumberSecond < evaluatedNumberThird)
+                Console.WriteLine("{0},{1},{2}", evaluatedNumberFirst,evaluatedNumberSecond,evaluatedNumberThird);
+                else if (evaluatedNumberFirst < evaluatedNumberThird)
+                Console.WriteLine("{0},{1},{2}", evaluatedNumberFirst, evaluatedNumberThird, evaluatedNumberSecond);
+            }
+
+            else
+            {
+                if (evaluatedNumberSecond < evaluatedNumberThird)
+                    Console.WriteLine("{0},{1},{2}", evaluatedNumberSecond, evaluatedNumberFirst, evaluatedNumberThird);
+                else if (evaluatedNumberFirst < evaluatedNumberThird)
+                    Console.WriteLine("{0},{1},{2}", evaluatedNumberSecond, evaluatedNumberThird, evaluatedNumberFirst);
+                
+            }
+
+            // Wait for user input
+            Console.ReadKey();
+        }
+    }
+}
