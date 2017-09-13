591ec9462a1aa4a9b0ff71f8beea153359b7b716
leinoka1 <kari.v.leino@gmail.com>
2017-09-13 14:47:14 +0300

    Added Task2 loop-statements

---
+++ loop-statements/loop-statements/Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            {
+                // Prompt user
+                // Read user first input
+
+                Console.Write("Input:");
+
+                string userInputN;
+                int evaluatedNumberN;
+                userInputN = Console.ReadLine();
+                int.TryParse(userInputN, out evaluatedNumberN);
+
+                // Confirm N
+                Console.WriteLine("You entered {0}", evaluatedNumberN);
+                if (evaluatedNumberN > 1)
+                {
+                    int loopCount = 0;
+                    long sumOutput = 0;
+                    while (loopCount < evaluatedNumberN)
+                    {
+                        loopCount++;
+                        sumOutput = loopCount + sumOutput;
+                    }
+
+                    Console.WriteLine("Output: {0}", sumOutput);
+
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Määrittelemätön");
+                }
+
+
+
+                // Wait for user input
+                Console.ReadKey();
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
