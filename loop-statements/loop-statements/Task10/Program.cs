07ea062634898402cf217efaf6dac0a4c03e6db6
leinoka1 <kari.v.leino@gmail.com>
2017-09-15 14:48:15 +0300

    Task10 added

---
+++ loop-statements/loop-statements/Task10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tipping
+            // 13 games
+            int loopCount = 0;
+            Random rnd = new Random();
+            double randomNumber = 0;
+            while (loopCount < 13)
+            {
+                loopCount++;
+                randomNumber = rnd.NextDouble();
+                if (randomNumber < 0.4)
+                {
+                    Console.WriteLine("{0}. 1", loopCount);
+                }
+                else if (randomNumber < 0.6)
+                {
+
+                    Console.WriteLine("{0}. X", loopCount);
+                }
+                else
+                {
+                    Console.WriteLine("{0}. 2", loopCount);
+                }
+                                           
+            }
+
+            // Wait for user input
+            Console.ReadKey();
+        }
+    }
+}
