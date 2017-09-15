885a3777ce71da5f5b5ad3e00128211dc3e289ea
leinoka1 <kari.v.leino@gmail.com>
2017-09-15 07:48:39 +0300

    Task 5 added

---
+++ loop-statements/loop-statements/Task5/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sum of serie for odd and even numbers
+            // Accepts also negative númbers
+            // Firts input
+            Console.Write("Input:");
+            string userInputN;
+            int evaluatedNumberN;
+            userInputN = Console.ReadLine();
+            int.TryParse(userInputN, out evaluatedNumberN);
+
+            // Confirm N
+            Console.WriteLine("You entered {0}", evaluatedNumberN);
+            if (evaluatedNumberN > 1)
+            {
+                int loopCount = 0;
+                int sumOutputEven = 0;
+                int sumOutputOdd = 0;
+                while (loopCount < evaluatedNumberN)
+                {
+                    loopCount++;
+                    if (loopCount % 2 == 0)
+                    {
+                        sumOutputEven = loopCount + sumOutputEven;
+
+                    }
+
+                    else
+                    {
+                        sumOutputOdd = loopCount + sumOutputOdd;
+
+                    }
+
+                }
+
+                Console.WriteLine("Output: Parittomien summa = {0}, Parillisten summa = {1}", sumOutputOdd, sumOutputEven);
+
+
+            }
+            else if (evaluatedNumberN < -1)
+            {
+                evaluatedNumberN = Math.Abs(evaluatedNumberN);
+                int loopCount = 0;
+                int sumOutputEven = 0;
+                int sumOutputOdd = 0;
+                while (loopCount < evaluatedNumberN)
+                {
+                    loopCount++;
+                    if (loopCount % 2 == 0)
+                    {
+                        sumOutputEven = loopCount + sumOutputEven;
+
+                    }
+
+                    else
+                    {
+                        sumOutputOdd = loopCount + sumOutputOdd;
+
+                    }
+
+                }
+                Console.WriteLine("Output: Parittomien summa = {0}, Parillisten summa = {1}", -sumOutputOdd, -sumOutputEven);
+            }
+
+            else
+            {
+
+                Console.WriteLine("Määrittelemätön");
+            }
+
+
+
+            // Wait for user input
+            Console.ReadKey();
+
+        }
+    }
+}
