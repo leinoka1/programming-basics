f990c53a270055340c527017afc6a441d672ed3c
leinoka1 <kari.v.leino@gmail.com>
2017-09-15 11:45:38 +0300

    Task9 added

---
+++ loop-statements/loop-statements/Task9/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Throw a coin n times
+            // Prompt user
+            // Read user first input
+
+            Console.Write("Please enter Number of throws:");
+
+            string userInputN;
+            int evaluatedNumberN;
+            userInputN = Console.ReadLine();
+            int.TryParse(userInputN, out evaluatedNumberN);
+
+            // Confirm N
+            // Console.WriteLine("You entered {0}", evaluatedNumberN);
+            int randomNumber = 0;
+            Random rnd = new Random();
+            if (evaluatedNumberN > 1)
+            {
+                int loopCount = 0;
+                int sumOutput = 0;
+                int headsN = 0;
+                int tailsN = 0;
+                while (loopCount < evaluatedNumberN)
+                {
+                    loopCount++;
+                    randomNumber = rnd.Next(2);
+                    if (randomNumber == 0)
+                    {
+                        headsN++;
+                    }
+                    else
+                    {
+
+                        tailsN++;
+                    }
+                   
+                    
+                }
+                Console.Write("Heads: {0} and Tails: {1}", headsN, tailsN);
+
+
+
+            }
+            else
+            {
+
+                Console.WriteLine("No throws!");
+            }
+
+
+
+            // Wait for user input
+            Console.ReadKey();
+        }
+    }
+}
