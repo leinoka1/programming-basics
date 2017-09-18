9e03635bbeed1fdf091bd714cdb80fb62156a9b3
leinoka1 <kari.v.leino@gmail.com>
2017-09-19 00:34:26 +0300

    added Task2 functions-subprograms/Task2/Program.cs

--- functions-subprograms/Task2/Program.cs
+++ functions-subprograms/Task2/Program.cs
@@ -6,9 +6,27 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
+            string firstNumber, secondNumber;
+            int evaluatedNumberFirst, evaluatedNumberSecond;
+            Console.Write("Input firts number: ");
+            firstNumber = Console.ReadLine();
+            int.TryParse(firstNumber, out evaluatedNumberFirst);
+            Console.Write("Input second number: ");
+            secondNumber = Console.ReadLine();
+            int.TryParse(secondNumber, out evaluatedNumberSecond);
+            int Minimi(int number1, int number2)
+                {
+                    if (number1 <= number2)
+                    {
+                        return number1;
+                    }
+                    else
+                    {
+                        return number2;
+                    }
+                }
+                Console.WriteLine("Min is {0}", Minimi(evaluatedNumberFirst,evaluatedNumberSecond));
+                Console.ReadKey();
         }
-
     }
 }
