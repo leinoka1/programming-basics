3fdea79a0a1eea411dd08e952256ac6cbaa19a60
leinoka1 <kari.v.leino@gmail.com>
2017-09-19 14:59:30 +0300

    Added task1 string-handling.sln

---
+++ string-handling/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            Console.Write("Enter a string: ");
+            userInput = Console.ReadLine();
+            Console.WriteLine("You entered {0} characters",userInput.Length);
+            Console.ReadKey();
+        }
+    }
+}
