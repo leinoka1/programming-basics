e38900b2d69cfdd5ad2931c5553076564cae9ab0
leinoka1 <kari.v.leino@gmail.com>
2017-09-19 15:05:54 +0300

    Addded Task2 string-handling/Task2/Program.cs

---
+++ string-handling/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            Console.Write("Enter a string: ");
+            userInput = Console.ReadLine();
+            Console.WriteLine("You entered {0}", userInput.Replace ("e","@"));
+            Console.ReadKey();
+        }
+    }
+}
