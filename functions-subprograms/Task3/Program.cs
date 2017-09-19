da0c9c6f31be64ee37af50bcb00474f74835f593
leinoka1 <kari.v.leino@gmail.com>
2017-09-19 13:11:13 +0300

    Task3 added

--- functions-subprograms/Task3/Program.cs
+++ functions-subprograms/Task3/Program.cs
@@ -6,7 +6,36 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string number;
+            int minimiumNumber, maximiumNumber;
+            minimiumNumber = 5;
+            maximiumNumber = 10;
+
+            Console.Write("Input number between {0} and {1}: ", minimiumNumber, maximiumNumber);
+            number = Console.ReadLine();
+            IsBetweenTwoNumbers(minimiumNumber, maximiumNumber, number);
+            Console.ReadKey();
         }
-    }
+       static int IsBetweenTwoNumbers(int minNumber, int maxNumber, string numberString)
+        {
+            int numberN;
+            bool isNumeric = int.TryParse(numberString, out numberN);
+            while (isNumeric == false)
+            {
+                Console.WriteLine("Given number {0} is not a number. Please enter a number: ", numberString);
+                numberString = Console.ReadLine();
+                Console.WriteLine("annoit {0}", numberString);
+                isNumeric = int.TryParse(numberString, out numberN);
+            }
+            int.TryParse(numberString, out numberN);
+            while (numberN >= maxNumber || numberN <= minNumber)
+            {
+                Console.Write("Input number {0} is not between and {1} - {2}. Please enter again: ", numberN, minNumber, maxNumber);
+                numberString = Console.ReadLine();
+                int.TryParse(numberString, out numberN);
+            }
+            Console.WriteLine("Given number is {0} and is between {1} - {2}", numberN, minNumber, maxNumber);
+            return numberN;
+        }
+    }            
 }
