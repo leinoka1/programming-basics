f7a561b99e38fe0999895465b42f6a8c8974cab6
leinoka1 <kari.v.leino@gmail.com>
2017-09-20 12:27:01 +0300

    Added Task4 string-handling/Task4/Program.cs

---
+++ string-handling/Task4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Palindrom tester
+            string userInput;
+            string userInputPlainText=String.Empty;
+            string reverseOrder = null;
+            Console.Write("Enter a string: ");
+            userInput = Console.ReadLine().ToUpper();
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                int j = userInput.Length - i;
+                if (Char.IsLetter(userInput[userInput.Length - j ]))
+                userInputPlainText = userInputPlainText + userInput[userInput.Length - j ];
+            }
+            Console.WriteLine("Your string as a plain text {0} ", userInputPlainText);
+            for (int i = 0; i < userInputPlainText.Length; i++)
+            {
+                reverseOrder = reverseOrder + userInputPlainText[userInputPlainText.Length - i - 1];
+            }
+            Console.WriteLine("Your string backwards is {0} ", reverseOrder);
+            if (reverseOrder == userInputPlainText)
+            {
+                Console.WriteLine("Given string is a palindrom!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Given string is NOT a palindrom!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
