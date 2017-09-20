a9b22e1d592c64203d50eeb2df6f68c0b1e430d2
leinoka1 <kari.v.leino@gmail.com>
2017-09-20 14:23:28 +0300

    Added Object exercise

---
+++ CarsObject/CarsObject/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarsObject
+{
+    class Car
+    {
+        // Private variables/fields
+        private string _originalColor;
+        // Properties
+        public string Color { get; set; }
+        public string Brand { get; }
+        public string Model { get; set; }
+        public double Speed { get; set; }
+        // Constructor
+        public Car(string color, string brand, string model)
+        {
+            Color = color;
+            Brand = brand;
+            Model = model;
+            Speed = 0;
+            _originalColor = color;
+        }
+        // Function
+        public void Accelerate()
+        {
+            Speed = Speed + 10;
+        }
+        public void Accelerate(double value)
+        {
+            Speed = Speed + value;
+        }
+        // Slowdown
+        public void Slowdown(double value)
+        {
+            Speed = Speed - value;
+        }
+        public override string ToString()
+        {
+            return $"Brand: {Brand}, Color: {Color}, Model: {Model}, Speed: {Speed}";
+        }
+    }
+}
---
+++ CarsObject/CarsObject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CarsObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string userInput = String.Empty;
+            int speed;
+            Console.WriteLine("Hello Cars");
+            Car car = new Car("blue", "Volvo", "V50");
+            Console.WriteLine(car);
+            /*car.Accelerate();
+            car.Accelerate(15);
+            car.Slowdown(18);
+            Console.WriteLine(car);
+            car.Color = "red";
+            Console.WriteLine(car);
+            */
+            Console.ReadLine();
+            Console.Write("Paljonko tiputetaan huippunopeutta:", userInput);
+            userInput = Console.ReadLine();
+            int.TryParse(userInput, out speed);
+            car.Slowdown(speed);
+            Console.WriteLine(car);
+            Console.ReadLine();
+        }
+    }
+}
